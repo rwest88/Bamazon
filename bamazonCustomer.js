585d3d5acc655e71a90d2ef1b13f02dc7a5dc03a
Ryan Weston <rwest88.va@gmail.com>
2018-11-07 01:59:32 -0500

    broke code into functions for easier read, fixed decimal issues for prices

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,6 +16,7 @@ connection.connect((err) => {
 });
 
 function makeTable() {
+  
   connection.query('SELECT * FROM products', (err, res) => {
     console.log('id || Product Name                                                                   || Department           || Price    || Quantity       ');
     console.log('===========================================================================================================================================');
@@ -28,13 +29,12 @@ function makeTable() {
       console.log(res[i].item_id + ' || ' + res[i].product_name + ' || ' + res[i].department_name + ' || ' + price + ' || ' + res[i].stock_quantity + ' in stock');
     }
     console.log('===========================================================================================================================================\n');
-    promptCustomer(res);
+    promptCustomerForItem(res);
   });
 }
 
-function promptCustomer(products) {
+function promptCustomerForItem(products) {
 
-  // Prompt customer for item
   inquirer.prompt({
     type: 'input',
     name: 'choice',
@@ -52,51 +52,61 @@ function promptCustomer(products) {
     }
     console.log('You chose ' + products[res.choice].product_name + ".\n");
 
-    // Prompt customer for quantity
-    inquirer.prompt({
-      type: 'input',
-      name: 'quantity',
-      message: "How many?",
-      filter: (input) => parseInt(input, 10),
-      validate: (input) => {
-        if (Number.isNaN(input)) return 'Enter a number!';
-        else if (input > products[res.choice].stock_quantity) return 'There\'s not enough in stock!';
-        return true;
-      }
-    }).then((res2) => {
-      connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', 
-      [products[res.choice].stock_quantity - res2.quantity, products[res.choice].item_id],
-      function(err, res3) {
-        if (err) return console.log(err);
-        chargeCustomer(products, res, res2)
-
-        // prompt customer for continue
-        inquirer.prompt({
-          type: 'input',
-          name: 'choice',
-          message: 'Would you like to make another purchase? (Y/N):'
-        }).then((res4) => {
-          if (res4.choice.toLowerCase() == 'y') makeTable();
-          else process.exit();
-        });
-      });
-    });
+    promptCustomerForQuantity(products[res.choice]);
   });
 }
 
-function chargeCustomer(products, res, res2) {
-  var tax = parseInt(products[res.choice].price * res2.quantity * 0.05) / 100;
-  var subtotal = parseInt(products[res.choice].price * 100) / 100 * res2.quantity / 100;
-  var total = subtotal + tax;
-  console.log('\nYou just purchased (' + res2.quantity + '):');
-  console.log(products[res.choice].product_name + " ($" + products[res.choice].price / 100 + ")");
-  console.log('x ' + res2.quantity 
-    + "..........................................................................($" 
+function promptCustomerForQuantity(product) {
+
+  inquirer.prompt({
+    type: 'input',
+    name: 'quantity',
+    message: "How many?",
+    filter: (input) => parseInt(input, 10),
+    validate: (input) => {
+      if (Number.isNaN(input)) return 'Enter a number!';
+      else if (input > product.stock_quantity) return 'There\'s not enough in stock!';
+      return true;
+    }
+  }).then((userInput) => updateProductsSetQuantity(product, userInput));
+}
+
+function updateProductsSetQuantity(product, userInput) {
+
+  connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', 
+    [product.stock_quantity - userInput.quantity, product.item_id],
+    function(err, res) {
+
+      if (err) return console.log(err);
+
+      chargeCustomer(product, userInput.quantity);
+    });
+}
+
+function chargeCustomer(product, quantity) {
+
+  var subtotal = (product.price / 100 * quantity).toFixed(2);
+  var tax = (product.price * quantity * 0.05 / 100).toFixed(2);
+  var total = (parseFloat(subtotal) + parseFloat(tax)).toFixed(2);
+  console.log('\nYou just purchased (' + quantity + '):');
+  console.log(product.product_name + " ($" + (product.price / 100).toFixed(2) + ")");
+  console.log('x ' + quantity 
+    + "............................................................................($" 
     + subtotal + ")");
-  console.log("Sales tax:.....................................................................($" 
+  console.log("Sales tax:......................................................................($" 
     + tax + ")");
-  console.log("Total:........................................................................($" 
+  console.log("Total:.........................................................................($" 
     + total + ")\n");
   console.log("We've just charged $" 
     + total + " to your credit card. You will receive no email confirmation. Thanks!\n");
+
+  // prompt customer for continue
+  inquirer.prompt({
+    type: 'input',
+    name: 'choice',
+    message: 'Would you like to make another purchase? (Y/N):'
+  }).then((res) => {
+    if (res.choice.toLowerCase() == 'y') makeTable();
+    process.exit();
+  });
 }
